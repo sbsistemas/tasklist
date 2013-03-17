ca116dca1242793a9aa55b7335e9a713fb05888d
sbsistemas <sbsistemas@yahoo.com.br>
2013-03-17 19:05:26 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pendencias::Application.config.secret_token = '4d47c4aa2ab4d2507e8e6c6553ce3b85f86c9ad532aa7b68ee0db0c08ad6265e99851e1e14107c12d5f1cc18b19916530951be8714824eb52922b6732ab334ae'
